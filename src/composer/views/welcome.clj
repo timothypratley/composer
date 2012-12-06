26e21224b473b3fbcd2b5d9c056d7a1d09c2b479
Timothy Pratley <timothypratley@gmail.com>
2012-12-06 15:59:47 -0800

    angularizing

--- src/composer/views/welcome.clj
+++ src/composer/views/welcome.clj
@@ -1,21 +1,46 @@
 (ns composer.views.welcome
-  (:require [composer.views.common :as common])
   (:use [noir.core :only [defpage]]
         [hiccup.form]
+        [hiccup.page]
+        [hiccup.core]
         [composer.models.messaging :only [consume]]))
 
-(defpage "/welcome" []
-         (common/layout
+(defpage "/" []
+         (html5
+           [:head
+             [:title "composer"]
+             [:meta {:name "viewport"
+                     :content "width=device-width"
+                     :initial-scale "1.0"}]
+             [:link {:rel "icon"
+                     :href "/img/favicon.ico"
+                     :type "image/x-icon"}]
+             [:link {:rel "shortcut"
+                     :href "/img/favicon.ico"
+                     :type "image/x-icon"}]
+             (include-css "//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css")
+             (include-css "/css/composer.css")]
+           [:body
+             [:div.ng-view "Loading..."]
+
+             (include-js "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js")
+             (include-js "//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js")
+             (include-js "//ajax.googleapis.com/ajax/libs/angularjs/1.0.2/angular.min.js")
+             (include-js "/js/controllers.js")
+             (include-js "/js/composer.js")]))
+
+(defpage "/about" []
+         (html
            [:p "Welcome to composer"]))
 
 (defpage "/message" []
-         (common/layout
+         (html
            (form-to [:post "/m"]
                     (text-area "message")
                     (submit-button "submit"))))
 
 (defpage [:post "/m"] {:keys [message]}
          (if-let [result (consume message)]
-           (common/layout result)
-           (common/layout "Error")))
+           (html result)
+           (html "Error")))
 
